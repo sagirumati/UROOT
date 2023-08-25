7a0a9a1dd704168472c490d19198c92b5afd9ff7
sagirumati <smatiweb@gmail.com>
2023-08-25 08:44:51 +0100

    updated 2023-08-25

--- R/uroot.R
+++ R/uroot.R
@@ -1,20 +1,32 @@
 # Hello, world!
 #
 # This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-#| @import EviewsR
+#' Conduct unit root test using `EViews` routines
+#'
+#' Use this function to conduct unit root test using `EViews` routines
 
+#' @return An EViews workfile
+#'
+#' @examples library(EviewsR)
+#' \dontrun{
+#' demo(exec_commands)
+#'
+#' eviews_graph(wf="exec_commands",page = "eviewspage1",series="x y",mode = "overwrite",
+#' graph_options = "m")
+#'
+#' # Create graph(s) from dataframe
+#'
+#' Data=data.frame(x=cumsum(rnorm(100)),y=cumsum(rnorm(100)))
+#'
+#' eviews_graph(series=Data,start_date=1990,frequency="m")
+#'
+#' # Create graphs in one frame (group=TRUE)
+#'
+#' eviews_graph(series=Data,group=TRUE,start_date="1990Q4",frequency="Q")
+#'}
+#' @family important functions
+#' @keywords documentation
+#' @export
 
 uroot <- function(x,test=c("adf","pp"),info="sic") {
   if(is.data.frame(x)) variables=colnames(x) else variables="uroot"
